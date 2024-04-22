f39575c90f390d9768f1117c2eb50ed2b91aa1fa
Hery Ruiz <heryrdev@gmail.com>
2024-04-21 23:52:03 -0400

    finsiehd noti

--- src/pages/home/Noti.jsx
+++ src/pages/home/Noti.jsx
@@ -1,49 +1,58 @@
 import React from "react";
 import "./styles/Noti.css";
 import ray from "../images/ray.jpg";
+import { ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Noti() {
   return (
     <section id="noti">
       <div className="noti__container">
         <div className="noti__left">
-
           <div className="noti__item">
-            <img src={ray} alt="Owner Image" className="noti__image"/>
+            <img src={ray} alt="Owner Image" className="noti__image" />
             <div className="noti__action">
               <p className="noti__title">Ray has booked a session.</p>
-              <p className="noti__des">Session was booked for April 2nd.
-              Ray has added extra details.
+              <p className="noti__des">
+                Session was booked for April 2nd. Ray has added extra details.
               </p>
             </div>
           </div>
 
           <div className="noti__item noti__two">
-            <img src={ray} alt="Owner Image" className="noti__image"/>
+            <img src={ray} alt="Owner Image" className="noti__image" />
             <div className="noti__action">
               <p className="noti__title">Ray has booked a session.</p>
-              <p className="noti__des">Session was booked for April 2nd.
-              Ray has added extra details.
+              <p className="noti__des">
+                Session was booked for April 2nd. Ray has added extra details.
               </p>
             </div>
           </div>
 
-          <div className="noti__item">
-            <img src={ray} alt="Owner Image" className="noti__image"/>
+          <div className="noti__item noti__three">
+            <img src={ray} alt="Owner Image" className="noti__image" />
             <div className="noti__action">
               <p className="noti__title">Ray has booked a session.</p>
-              <p className="noti__des">Session was booked for April 2nd.
-              Ray has added extra details.
+              <p className="noti__des">
+                Session was booked for April 2nd. Ray has added extra details.
               </p>
             </div>
           </div>
         </div>
         <div className="noti__right">
-          <p className="noti__head"></p>
-          <p className="noti__body"></p>
-          <div className="noti__redirect">
-
-          </div>
+          <p className="noti__head">
+            Get all the <span className="noti__outline">alerts</span> you
+            need.
+          </p>
+          <p className="noti__body">
+            Effortlessly manage your notifications with our intuitive pet
+            trainer app. Stay in control of your pet's training schedule and
+            progress, while customizing alerts to suit your preferences.
+          </p>
+          <Link className="noti__redirect">
+            <p>Learn More</p>
+            <ArrowRight className="noti__icon" />
+          </Link>
           <p className="noti__redirect"></p>
         </div>
       </div>
